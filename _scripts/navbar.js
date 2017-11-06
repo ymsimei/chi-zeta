cf87ddee33cc6de21ef78204c7eb85c1ac91418a
Antonio Mistretta <ymsimei@gmail.com>
2017-11-06 13:27:00 -0500

    reverted nav js

--- _scripts/navbar.js
+++ _scripts/navbar.js
@@ -1,8 +1,6 @@
 let nav = document.getElementsByTagName("nav")[0];
 nav.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
-window.onscroll = changeNavColor()
-
-function changeNavColor() {
+window.onscroll = function (e) {
   if ( window.pageYOffset > 50 ) {
     nav.style.backgroundColor = "#2c4b81";
   } else {
