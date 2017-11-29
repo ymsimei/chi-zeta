2188cd5f4f2e42be05ab9d5f260262e06cd724c3
Antonio Mistretta <ymsimei@gmail.com>
2017-11-29 12:06:46 -0500

    removed sidebar

--- _site/_scripts/loadBrothers.js
+++ _site/_scripts/loadBrothers.js
@@ -225,10 +225,10 @@ var foundingValues = [
 ]
 
 function changeIMGs(array) {
-  newContent = "";
+  newContent = "<div class='col-md-12'><h3>Founding Class</h3><hr/></div>";
   for (var i = 0; i < array.length; i++) {
     member = array[i];
-    var newMemberHTML = "<div class='col-xs-6 col-md-3 headshot'><img src='"+member.imgURL+"'><h4>"+member.name+"</h4></div>";
+    var newMemberHTML = "<div class='col-6 col-md-2 headshot'><img src='"+member.imgURL+"'><h4>"+member.name+"</h4></div>";
 
 	console.log(newMemberHTML)
     var newContent = newContent.concat(newMemberHTML);
@@ -237,7 +237,7 @@ function changeIMGs(array) {
 }
 
 $('#alpha').click(function(){
-    document.getElementById("brother-class").innerHTML = "Spring 2017";
+    document.getElementById("brother-class").innerHTML = "Fall 2017";
     changeIMGs(alphaValues);
 });
 
