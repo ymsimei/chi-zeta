9b0b924a08a8d1547b77895c0b73a32c7194e8c2
Antonio Mistretta <ymsimei@gmail.com>
2017-11-29 20:59:26 -0500

    Added brother data as a json

--- _scripts/loadBrothers.js
+++ _scripts/loadBrothers.js
@@ -1,35 +1,34 @@
 ---
 ---
 
-var foundingValues = [
-    {% for image in site.static_files %}
-        {% if image.path contains 'headshots/founding' %}
-            {name: "{{image.name}}".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "{{ site.baseurl }}{{ image.path }}"},
-        {% endif %}
-    {% endfor %}
-]
+//RUNS ON LOAD
+loadJSON(function(response) {
+  // Parse JSON string into object
+  var brothers = JSON.parse(response);
+  displayBrotherData(brothers);
+ });
 
-function changeIMGs(array) {
-  newContent = "<div class='col-md-12'><h3>Founding Class</h3><hr/></div>";
-  for (var i = 0; i < array.length; i++) {
-    member = array[i];
-    var newMemberHTML = "<div class='col-6 col-sm-3 col-lg-2 headshot'><img class='headshot' src='"+member.imgURL+"'/><h4>"+member.name+"<a href='#'><img class='linkedin' src={{site.baseurl}}/_assets/svg/linkedin.svg></a></h4></div>";
-
-	console.log(newMemberHTML)
-    var newContent = newContent.concat(newMemberHTML);
-  }
-  document.getElementById("headshots").innerHTML = newContent;
+function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', '{{ site.baseurl }}/_scripts/brotherdata.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
 }
 
-$('#alpha').click(function(){
-    document.getElementById("brother-class").innerHTML = "Fall 2017";
-    changeIMGs(alphaValues);
-});
-
-$('#founding').click(function(){
-    document.getElementById("brother-class").innerHTML = "Spring 2017";
-    changeIMGs(foundingValues);
-});
-
-//RUNS ON LOAD
-changeIMGs(foundingValues);
+function displayBrotherData(dictionary) {
+  //console.log(dictionary);
+  var html = "";
+  for (var grade in dictionary) {
+    html += "<div class='col-12'><h4>"+grade+"</h4><hr/></div>"
+    for (var name in dictionary[grade]) {
+      html += "<div class='col-6 col-sm-3 col-lg-2 headshot'><img class='headshot' src='"+dictionary[grade][name]['headshot']+"'/><h4>"+name+"<a href='#'><img class='linkedin' src={{site.baseurl}}/_assets/svg/linkedin.svg></a></h4></div>"
+    }
+  }
+  document.getElementById("headshots").innerHTML = html;
+}
--- _site/_scripts/loadBrothers.js
+++ _site/_scripts/loadBrothers.js
@@ -1,252 +1,31 @@
-var foundingValues = [
-    
-        
-    
-        
-    
-        
-            {name: "Adit Patel.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Adit Patel.jpg"},
-        
-    
-        
-            {name: "Aidan McGourty.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Aidan McGourty.jpg"},
-        
-    
-        
-            {name: "Akash Sewani.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Akash Sewani.jpg"},
-        
-    
-        
-            {name: "Antonio Mistretta.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Antonio Mistretta.jpg"},
-        
-    
-        
-            {name: "Aris Rafael.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Aris Rafael.jpg"},
-        
-    
-        
-            {name: "Arman Moussavi.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Arman Moussavi.jpg"},
-        
-    
-        
-            {name: "Astha Sharma.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Astha Sharma.jpg"},
-        
-    
-        
-            {name: "Awais Qazi.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Awais Qazi.jpg"},
-        
-    
-        
-            {name: "Bindi Shah.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Bindi Shah.jpg"},
-        
-    
-        
-            {name: "Christina De Ramos.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Christina De Ramos.jpg"},
-        
-    
-        
-            {name: "Christopher Yap.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Christopher Yap.jpg"},
-        
-    
-        
-            {name: "Dhanush Sureshbabu.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Dhanush Sureshbabu.jpg"},
-        
-    
-        
-            {name: "Dron Patel.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Dron Patel.jpg"},
-        
-    
-        
-            {name: "Dylan Kumenap.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Dylan Kumenap.jpg"},
-        
-    
-        
-            {name: "Greg Buys.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Greg Buys.jpg"},
-        
-    
-        
-            {name: "Jacob Fattakhov.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Jacob Fattakhov.jpg"},
-        
-    
-        
-            {name: "Jacob Ponulak.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Jacob Ponulak.jpg"},
-        
-    
-        
-            {name: "Jaideep Patel.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Jaideep Patel.jpg"},
-        
-    
-        
-            {name: "Jermayne Noseal.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Jermayne Noseal.jpg"},
-        
-    
-        
-            {name: "Jessamine Mombay.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Jessamine Mombay.jpg"},
-        
-    
-        
-            {name: "John Shashaty.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/John Shashaty.jpg"},
-        
-    
-        
-            {name: "Joshua Wolfe.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Joshua Wolfe.jpg"},
-        
-    
-        
-            {name: "J~Ream Lewis.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/J~Ream Lewis.jpg"},
-        
-    
-        
-            {name: "Kristen King.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Kristen King.jpg"},
-        
-    
-        
-            {name: "Malay Patel.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Malay Patel.jpg"},
-        
-    
-        
-            {name: "Manogna Guduru.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Manogna Guduru.jpg"},
-        
-    
-        
-            {name: "Mansha Kohli.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Mansha Kohli.jpg"},
-        
-    
-        
-            {name: "Mansi Sheth.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Mansi Sheth.jpg"},
-        
-    
-        
-            {name: "Michael Vaks.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Michael Vaks.jpg"},
-        
-    
-        
-            {name: "Nikhil Gandhi.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Nikhil Gandhi.jpg"},
-        
-    
-        
-            {name: "Nithin  Dronavalli.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Nithin  Dronavalli.jpg"},
-        
-    
-        
-            {name: "Palash Jain.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Palash Jain.jpg"},
-        
-    
-        
-            {name: "Parth Vachhani.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Parth Vachhani.jpg"},
-        
-    
-        
-            {name: "Reza Wang-Lotfi.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Reza Wang-Lotfi.jpg"},
-        
-    
-        
-            {name: "Riya Kurian.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Riya Kurian.jpg"},
-        
-    
-        
-            {name: "Ross Jason Paayas-Pecson.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Ross Jason Paayas-Pecson.jpg"},
-        
-    
-        
-            {name: "Rushabh Mehta.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Rushabh Mehta.jpg"},
-        
-    
-        
-            {name: "Sanil Shah.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Sanil Shah.jpg"},
-        
-    
-        
-            {name: "Shail Shah.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Shail Shah.jpg"},
-        
-    
-        
-            {name: "Sheryl Jacob.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Sheryl Jacob.jpg"},
-        
-    
-        
-            {name: "Shivam Kanbur.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Shivam Kanbur.jpg"},
-        
-    
-        
-            {name: "Shravanthi Budhi.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Shravanthi Budhi.jpg"},
-        
-    
-        
-            {name: "Steve Diaz.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Steve Diaz.jpg"},
-        
-    
-        
-            {name: "Tapesh Nagarwal.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Tapesh Nagarwal.jpg"},
-        
-    
-        
-            {name: "Vaibhav Pothireddy.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Vaibhav Pothireddy.jpg"},
-        
-    
-        
-            {name: "Vaibhava Pendyala.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Vaibhava Pendyala.jpg"},
-        
-    
-        
-            {name: "Vasu Patel.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Vasu Patel.jpg"},
-        
-    
-        
-            {name: "Vinaya Thadhani.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Vinaya Thadhani.jpg"},
-        
-    
-        
-            {name: "William Joe McCann.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/William Joe McCann.jpg"},
-        
-    
-        
-            {name: "Zohaeb Atiq.jpg".replace(".jpg", "").replace("~", "'"), position: "Member", imgURL: "/_assets/jpg/headshots/founding/Zohaeb Atiq.jpg"},
-        
-    
-        
-    
-        
-    
-        
-    
-        
-    
-        
-    
-        
-    
-        
-    
-        
-    
-        
-    
-        
-    
-]
+//RUNS ON LOAD
+loadJSON(function(response) {
+  // Parse JSON string into object
+  var brothers = JSON.parse(response);
+  displayBrotherData(brothers);
+ });
 
-function changeIMGs(array) {
-  newContent = "<div class='col-md-12'><h3>Founding Class</h3><hr/></div>";
-  for (var i = 0; i < array.length; i++) {
-    member = array[i];
-    var newMemberHTML = "<div class='col-6 col-sm-3 col-lg-2 headshot'><img class='headshot' src='"+member.imgURL+"'/><h4>"+member.name+"<a href='#'><img class='linkedin' src=/_assets/svg/linkedin.svg></a></h4></div>";
-
-	console.log(newMemberHTML)
-    var newContent = newContent.concat(newMemberHTML);
-  }
-  document.getElementById("headshots").innerHTML = newContent;
+function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', '/_scripts/brotherdata.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
 }
 
-$('#alpha').click(function(){
-    document.getElementById("brother-class").innerHTML = "Fall 2017";
-    changeIMGs(alphaValues);
-});
-
-$('#founding').click(function(){
-    document.getElementById("brother-class").innerHTML = "Spring 2017";
-    changeIMGs(foundingValues);
-});
-
-//RUNS ON LOAD
-changeIMGs(foundingValues);
+function displayBrotherData(dictionary) {
+  //console.log(dictionary);
+  var html = "";
+  for (var grade in dictionary) {
+    html += "<div class='col-12'><h4>"+grade+"</h4><hr/></div>"
+    for (var name in dictionary[grade]) {
+      html += "<div class='col-6 col-sm-3 col-lg-2 headshot'><img class='headshot' src='"+dictionary[grade][name]['headshot']+"'/><h4>"+name+"<a href='#'><img class='linkedin' src=/_assets/svg/linkedin.svg></a></h4></div>"
+    }
+  }
+  document.getElementById("headshots").innerHTML = html;
+}
